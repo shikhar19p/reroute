03e7c26cbd7bed434690375f6d31e403cfff97c8
Narender Reddy <akshitareddybanala@gmail.com>
2026-07-03 13:39:15 +0530

    chore: bump versionCode to 21 so EAS auto-increments to 22

--- app.config.js
+++ app.config.js
@@ -28,7 +28,7 @@ export default {
     },
     android: {
       package: "com.rerouteaventures.app",
-      versionCode: 20,
+      versionCode: 21,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#F9F8EF"
